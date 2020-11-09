87fcf5d922a9e9dcd76c655ebb6db2bd0f3e0a7b
Emmanuel Jammes <emmanuel.jammes@ratpsmartsystems.com>
2020-11-09 11:25:06 +0100

    added tweet template in ini file

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ import tweepy
 # config
 #################################
 
-
 # logging 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
@@ -39,23 +38,23 @@ class WordGenerator():
                 try:
                     last_pos = int(f_last_pos.read())
                 except:
-                    logging.debug(f"last pos is not an integer")
+                    logging.debug("last pos is not an integer")
                     last_pos = -1
 
         else:
-            logging.debug(f"last pos file does not exists")
+            logging.debug("last pos file does not exists")
             last_pos = -1
         
-        logging.debug(f"last pos : {last_pos}")
+        logging.debug("last pos : %s" % last_pos)
         return last_pos
 
     def save_last_pos(self):
         with open(self.last_pos_file, "w") as f_last_pos:
-            f_last_pos.write(f"{self.cur_pos}")
+            f_last_pos.write("%s" % self.cur_pos)
 
     def get_next_word(self):
         f = open(self.words_file, 'r', encoding='utf8')
-        logging.debug(f"reading text file {f.name}")
+        logging.debug("reading text file %s" % f.name)
         for i, line in enumerate(f):
             if i == self.cur_pos:
                 return line.strip()
@@ -66,29 +65,37 @@ class WordGenerator():
 
 def main():
 
-    # config file
+    # config
     config = ConfigParser()
     config.read('bot.ini')
 
+    tweet_template =config["bot"]["tweet_template"]
+
     script_dir = Path(__file__).resolve().parent
-    words_file = script_dir / Path(config["bot"]["words_file"])
+    words_file     = script_dir / Path(config["bot"]["words_file"])
     last_post_file = script_dir / Path(config["bot"]["last_pos_file"])
 
-    word_gen = WordGenerator(words_file, last_post_file)
-    print(word_gen.get_next_word())
-    word_gen.save_last_pos()
+    api_key             = config["twitter"]["api_key"]
+    api_secret_key      = config["twitter"]["api_secret_key"]
+    access_token        = config["twitter"]["access_token"]
+    access_token_secret = config["twitter"]["access_token_secret"]
 
-    # Authenticate to Twitter
-    # auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-    auth = tweepy.OAuthHandler(config["twitter"]["api_key"], config["twitter"]["api_secret_key"])
-    # auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
-    auth.set_access_token(config["twitter"]["access_token"], config["twitter"]["access_token_secret"])
+    # twitter auth
+    auth = tweepy.OAuthHandler(api_key, api_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
 
-    # Create API object
     api = tweepy.API(auth)
 
-    # Post a tweet
-    api.update_status("Hello")
+    # get last tweeted word
+    word_gen = WordGenerator(words_file, last_post_file)
+    next_word = word_gen.get_next_word()
+
+    # post tweet
+    # api.update_status(config["twitter"]["tweet_template"] % next_word)
+    print(tweet_template.format(next_word))
+
+    # save pos
+    word_gen.save_last_pos()
 
 if __name__ == "__main__":
     main()
